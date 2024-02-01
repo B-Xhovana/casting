d5e8836d69cb9f3cfc7eefdff454a9f182f32334
Viviana <vivianaman378@gmail.com>
2024-02-01 17:03:57 +0100

    Aggiunti IndexController (+ relativi metodi in FilmDao, FilmService e FilmServiceImpl) e index.html
    Raul Bova Riposa in Pace.

--- src/main/java/it/corso/service/FilmService.java
+++ src/main/java/it/corso/service/FilmService.java
@@ -3,13 +3,21 @@ package it.corso.service;
 import java.util.List;
 import java.util.Map;
 
+
+
+
 import it.corso.model.Film;
 
 public interface FilmService {
 	
 	List<Film> getFilms();
 	
-    Map<String, List<Film>> getFilmsByGenere(List<Film> films);
-	Film getFilmById(int id);
+	
+	Film getFilmByID(int id);
+	//Medoto con stream per ottenere il catalogo dei film divisi per genenere in home page
+	Map<String, List<Film>> getFilmByGenere(List<Film> films);
+	String[] getListaGeneri();
+	
+	
 	
 }
